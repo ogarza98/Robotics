33f90b9cec4d6714e54cf5996b7dcbf3bc6e560c
Oscar Garza <ogarza98@live.com>
2020-04-12 17:24:23 -0500

    oscar

--- PrimaryActive.java
+++ PrimaryActive.java
@@ -29,6 +29,7 @@ public class PrimaryActive extends LinearOpMode {
     int nextZ;
     int StartDirection = 0; //INPUT START DIRECTION (0, 90, 180, 270)
     int FinalMove;
+    double degreeToTurn;
     
     
     
@@ -74,67 +75,90 @@ public class PrimaryActive extends LinearOpMode {
             //if(StartDirection == 0){ //Looping statement for start facing 0 degrees
             // Direction is clockwise from 0 to 270
             if(StartDirection == 0){
-            
-                if(nextX == (currentX - 1) && nextY == currentY && currentZ == 0){        // if x decreases & Y doesnt change & direction is 0; 
-                        robot.encoderDrive(robot.DRIVE_SPEED,  -5,    -5,    1.5);        //Forward 12 Inches
-                        currentZ = 0;
-                        telemetry.addData("Direction", currentZ);
-                        //telemetry.update();
-                    }
-                if(nextX == currentX && nextY == (currentY + 1) && currentZ == 0){        //if x doesnt change & Y increases & direction is 0;  
-                        robot.encoderDrive(robot.TURN_SPEED,   -3,     3,    1.75);        //Turn Right 90 degrees
-                        robot.encoderDrive(robot.DRIVE_SPEED, -10,   -10,    1.5);        //Forward 12 Inches
-                        currentZ = 90;
-                        telemetry.addData("Direction", currentZ);
-                        //telemetry.update();
-                    }
-                if(currentX == nextX && currentY == nextY + 1 && currentZ == 90){
-                    robot.encoderDrive(robot.DRIVE_SPEED,  -5,    -5,    1.5);         //Forward 12 Inches
-                    currentZ = 90;
-                    telemetry.addData("Direction", currentZ);
-                }
 
-                if((nextX == currentX) && (nextY == (currentY - 1)) && currentZ == 90 ){  // if x doesnt change & Y decreases & direction is 90; 
-                       robot.encoderDrive(robot.TURN_SPEED,    6,    -6,   1.55);         //Turn left 180 degrees to 270 degrees
-                       robot.encoderDrive(robot.DRIVE_SPEED,  -5,    -5,    1.5);         //Forward 12 Inches
-                       currentZ = 270;
-                       telemetry.addData("Direction", currentZ);
-                       //telemetry.update();
-                    }
-                if(nextX == (currentX - 1) && nextY == currentY && currentZ == 270){      //if x decreases & Y doesnt change & direction is 180;
-                       robot.encoderDrive(robot.TURN_SPEED,   -3,     3,    1.5);         //Turn Right 90 degrees
-                       robot.encoderDrive(robot.DRIVE_SPEED, -10,   -10,    1.5);         //Forward 12 Inches
-                       currentZ = 0;
-                       telemetry.addData("Direction", currentZ);
-                       //telemetry.update();
-                }
-                if(nextX == (currentX - 1) && nextY == currentY && currentZ == 90){       //if x decreases & Y doesnt change & direction is 90; 
-                       robot.encoderDrive(robot.TURN_SPEED,    3,    -3,   1.55);         //Turn left 90 degrees
-                       robot.encoderDrive(robot.DRIVE_SPEED, -10,   -10,    1.5);         //Forward 12 Inches
-                       currentZ = 0;
-                       telemetry.addData("Direction", currentZ);
-                       //telemetry.update();
-                }
-                if((nextX == currentX) && (nextY == (currentY - 1)) && currentZ == 0 ){   // if x doesnt change & Y decreases & direction is 0;  
-                       robot.encoderDrive(robot.TURN_SPEED,    3,    -3,   1.55);         //Turn left 90 degrees to 270
-                       robot.encoderDrive(robot.DRIVE_SPEED,  -5,    -5,    1.5);         //Forward 12 Inches
-                       currentZ = 270;
-                       telemetry.addData("Direction", currentZ);
-                       //telemetry.update();
-                    }
-                if(currentX == End_X && currentY == End_Y){
-                    FinalMove = i;
-                    break;
-                }
+                degreeToTurn=determineDegreeToTurn(current X, currentY, nextX, nextY, currentDegree);
+                moveRobot(degreeToTurn);
                 
             }
         
 
-         }
-         if(currentZ == 90){
-             robot.encoderDrive(robot.DRIVE_SPEED,  -5,    -5,    1.5);         //Forward 12 Inches
-         }
+         
+         
                 telemetry.update();
                 sleep(10000);
+         }
 }
+
+public double determineDegreeToTurn(int current X, int currentY, int nextX, int nextY, double currentDegree){
+
+    if(currentY==nextY))
+    {
+
+        if(nextX>currentX) targetDegree=0;
+
+        else targetDegree=180;
+        
+    }
+
+    else if (nextX==currentX) 
+    {
+         if(nextY>currentY) targetDegree=90;
+
+         else targetDegree=270
+         
+    }
+
+    if (degreeToTurn>180) degreeToTurn-=360;
+    if(degreeToTurn<-180) degreeToTurn+=360;
+
+    return degreeToTurn;
+
+    }
+
+public void moveRobot(double degreeToTurn){
+
+
+        switch (degreeToTurn)
+        
+        {
+            case 0:
+            robot.encoderDrive(robot.DRIVE_SPEED, -5, -5, 1.5);
+            currentZ = currentZ;
+            telemetry.addData(“Direction”, currentZ);
+
+
+            case 90:
+            robot.encoderDrive(robot.TURN_SPEED, -3, 3, 1.75);
+            robot.encoderDrive(robot.DRIVE_SPEED, -5, -5, 1.5);
+            int currentZ = currentZ + 90;
+            telemetry.addData(“Direction”, currentZ);
+
+
+            case -90:
+            robot.encoderDrive(robot.TURN_SPEED, 3, -3, 1.75);
+            robot.encoderDrive(robot.DRIVE_SPEED, -5, -5, 1.5);
+            int currentZ = currentZ - 90;
+            telemetry.addData(“Direction”, currentZ);
+
+
+            case 180:
+            robot.encoderDrive(robot.TURN_SPEED, -6, 6, 1.75);
+            robot.encoderDrive(robot.DRIVE_SPEED, -5, -5, 1.5);
+            int currentZ = currentZ + 180;
+            telemetry.addData(“Direction”, currentZ);
+
+
+            case -180:
+            robot.encoderDrive(robot.TURN_SPEED, 6, -6, 1.75);
+            robot.encoderDrive(robot.DRIVE_SPEED, -5, -5, 1.5);
+            int currentZ = currentZ - 180;
+            telemetry.addData(“Direction”, currentZ);
+
+
+        }
+
 }
+
+}
+
+
